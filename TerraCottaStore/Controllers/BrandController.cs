1dfb149de80095e4470aebad1971fecf984edb34
LaosPhu <119502732+LaosPhu@users.noreply.github.com>
2024-11-25 05:40:10 +0700

    p6

---
+++ TerraCottaStore/Controllers/BrandController.cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TerraCottaStore.Models;
+using TerraCottaStore.Repository;
+
+namespace TerraCottaStore.Controllers
+{
+	public class BrandController : Controller
+	{
+		private readonly DataContext _datacontext;
+
+		public BrandController(DataContext dataContext) 
+		{
+		 _datacontext = dataContext;
+		}
+
+		public async Task<IActionResult> Index(string Slug = "")
+		{
+			BrandModel brand = _datacontext.Brands.Where(c => c.slug == Slug).FirstOrDefault();
+			if (brand == null)
+			{
+				return RedirectToAction("Index");
+			}
+
+			var productByBrand = _datacontext.Products.Where(c => c.CategoryID == brand.Id);
+
+			return View(await productByBrand.OrderByDescending(C => C.Id).ToListAsync());
+		}
+	}
+}
--- TerraCottaStore/Controllers/ProductController.cs
+++ TerraCottaStore/Controllers/ProductController.cs
@@ -1,16 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TerraCottaStore.Repository;
 
 namespace TerraCottaStore.Controllers
 {
 	public class ProductController : Controller
 	{
+		private readonly DataContext _datacontext;
+
+		public ProductController(DataContext dataContext) 
+		{
+		 _datacontext = dataContext;
+		}
 		public IActionResult index()
 		{
 			return View();
 		}
-		public IActionResult ProductDetail()
-		{
-			return View();
+		public async Task<IActionResult> ProductDetail(int Id )
+		{	
+			if (Id == null)
+			{
+				return RedirectToAction("Index");
+			}
+			var productdetail = _datacontext.Products.Where(c => c.Id == Id).FirstOrDefault();
+			return View(productdetail);
 		}
 
 	}
